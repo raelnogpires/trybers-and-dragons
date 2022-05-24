3a26029655ce2f05be4ccb42c03f08a9b08f7633
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-24 18:55:28 -0300

    feat: create Warrior class

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private static _instanceCounter = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._instanceCounter += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior._instanceCounter;
+  }
+}
