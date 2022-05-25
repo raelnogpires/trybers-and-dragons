36283478fa2b41eabaef056f4e735e9483b2042e
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-25 16:33:20 -0300

    feat: create class Monster, Characters can now attack Monsters(SimpleFighters). finish task 9

---
+++ src/Monster.ts
@@ -0,0 +1,30 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number { return this._lifePoints; }
+
+  get strength(): number { return this._strength; }
+
+  public receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+
+    if (damage > 0) this._lifePoints -= damage;
+
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  public attack(enemy: SimpleFighter): void {
+    const damage = this._strength;
+    enemy.receiveDamage(damage);
+  }
+}
