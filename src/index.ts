2880aed0db182e2ed043f4deb2a0d1dcdebebf66
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-25 20:01:48 -0300

    please evalutor, evaluate

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,7 @@ const pvp = new PVP(player2, player3);
 const pve = new PVE(player1, [monster1, monster2]);
 
 const runBattles = (battles: Battle[]) => {
-  battles.forEach((b) => {
-    b.fight();
-  });
+  battles.forEach((b) => b.fight());
 };
 
 export {
